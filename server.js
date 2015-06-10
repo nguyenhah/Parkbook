292efdc8fe0011371d30a82b2ec3b877ff0c0401
Vincent Chan <v.chan36@gmail.com>
2015-06-10 14:55:47 -0700

    linked remote database and added test park information

--- server.js
+++ server.js
@@ -9,22 +9,35 @@ app.use(cors());
 
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://cl:cl@ds034878.mongolab.com:34878/contactlist/parklist');
+mongoose.connect('mongodb://pb:pb@ds041992.mongolab.com:41992/parkbook');
 
-var Park = mongoose.model('Park', {name: String});
+var Park = mongoose.model('Park', {
+    name: String,
+    streetNumber: String,
+    streetName: String,
+    lat: Number,
+    lon: Number
+});
 
-var park = new Park({name: "Arbutus"});
+
+var arbutusPark = new Park({
+    name: "Arbutus Village",
+    streetNumber: "4202",
+    streetName: "Valley Drive",
+    lat: 49.249783,
+    lon: -123.155250
+});
 
 
 
  //ADD NEW ITEM TO DATABASE VIA MONGOOSE
-park.save(function (err) {
-    if (err) {
-        console.log("failed");
-    } else {
-        console.log("saved");
-    }
-});
+//arbutusPark.save(function (err) {
+//    if (err) {
+//        console.log("failed");
+//    } else {
+//        console.log("saved");
+//    }
+//});
 
 app.get("/", function (req, res) {
     Product.find(function (err, products) {
