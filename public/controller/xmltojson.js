1bc8c019229281077a76ac675b5da009c3f8c588
Ha Nguyen <nguyenhasc@gmail.com>
2015-06-11 18:23:02 -0700

    added xmltojson parser, semi-fuctional

---
+++ public/controller/xmltojson.js
@@ -0,0 +1,21 @@
+/**
+ * Created by hazai on 11/06/15.
+ */
+var fs = require('fs'),
+    xml2js = require('xml2js');
+var filePath = 'data/temp/parkdata.xml';
+var parser = new xml2js.Parser();
+try {
+    var fileData = fs.readFileSync(filePath, 'ascii');
+
+    var parser = new xml2js.Parser();
+    parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
+        var json = JSON.stringify(result);
+        console.log(result);
+    });
+
+    console.log("File '" + filePath + "/ was successfully read.\n");
+} catch (ex) {
+    console.log("Unable to read file '" + filePath + "'.");
+    console.log(ex);
+}
--- server.js
+++ server.js
@@ -57,6 +57,5 @@ app.post("/add", function(req, res) {
     })
 });
 
-
 app.listen(3000);
 console.log("App running on port 3000");
