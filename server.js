1f96a3fca168a66c20c9ed49060c94202106e608
yves-chan <chanyves@gmail.com>
2015-06-17 00:25:58 -0700

    Added so that deployed app displays index.html

--- server.js
+++ server.js
@@ -10,17 +10,31 @@ var admin = require("./admin");
 var Park = require("./models/parkModel");
 var parkModel = Park.parkModel;
 
+//deployment to heroku
+var http = require('http');
+//Listen to env port or 3000 and set it in express
+var port = Number(process.env.PORT || 3000);
+var path = require("path");
+app.set('port', port);
+//app.set('views', __dirname);
+//app.engine('html',require('ejs').renderFile);
+//app.set('view engine', 'ejs');
+
+
+var server = http.createServer(app);
+
 app.use(cors());
 app.use(bodyParser());
+app.use(express.static(__dirname+'/public'));
 
 
 mongoose.connect('mongodb://pb:pb@ds041992.mongolab.com:41992/parkbook');
 
-
 app.get("/", function (req, res) {
     parkModel.find(function (err, parks) {
         res.send(parks);
-    })
+    });
+    res.render('index.html');
 });
 
 app.post("/add", function(req, res) {
@@ -57,5 +71,8 @@ app.get("/searchall", function(req, res) {
 });
 
 
-app.listen(3000);
-console.log("App running on port 3000");
+//app.listen(port);
+//console.log("App running on port 3000");
+server.listen(port, function() {
+    console.log("Node app is running on port:" + port);
+});
