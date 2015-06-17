3189a36dadcdddb4b24d95f4c72f86a0279bab3a
yveschan <chanyves@gmail.com>
2015-06-17 11:23:10 -0700

    Made some changes for deployment to heroku

--- server.js
+++ server.js
@@ -16,25 +16,21 @@ var http = require('http');
 var port = Number(process.env.PORT || 3000);
 var path = require("path");
 app.set('port', port);
-//app.set('views', __dirname);
-//app.engine('html',require('ejs').renderFile);
-//app.set('view engine', 'ejs');
 
 
 var server = http.createServer(app);
 
 app.use(cors());
 app.use(bodyParser());
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname +'/public'));
 
 
 mongoose.connect('mongodb://pb:pb@ds041992.mongolab.com:41992/parkbook');
 
-app.get("/", function (req, res) {
+app.get("/home", function (req, res) {
     parkModel.find(function (err, parks) {
         res.send(parks);
     });
-    res.render('index.html');
 });
 
 app.post("/add", function(req, res) {
@@ -70,9 +66,6 @@ app.get("/searchall", function(req, res) {
     res.send();
 });
 
-
-//app.listen(port);
-//console.log("App running on port 3000");
 server.listen(port, function() {
     console.log("Node app is running on port:" + port);
 });
