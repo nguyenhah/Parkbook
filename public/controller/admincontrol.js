f49190ecbb2a15f6a4bf8fddecfafc695a6e4e98
yves-chan~ <chanyves@gmail.com>
2015-07-13 15:26:30 -0700

    changed back to dev mode

---
+++ public/controller/admincontrol.js
@@ -0,0 +1,24 @@
+/**
+ * Created by yves on 13/07/15.
+ */
+
+var parkbook = angular.module("parkbook");
+
+parkbook.controller("AdminCtrl", ['$http', '$scope','admin', function ($http, $scope, admin) {
+    var url = "http://localhost:3000";
+    //var url = "https://parkbook.herokuapp.com";
+
+    //this function is called inside HTML
+    //the http call is tagged with "/download" and sent to server.js
+    //server.js calls the app.get() with the "/download" tag
+    $scope.importParks = function() {
+        console.log("clicked import");
+        $http.get(url + "/download").success(function() {
+            console.log("inside success");
+            //loadParks();
+        })
+
+    };
+
+
+}]);
--- public/controller/app.js
+++ public/controller/app.js
@@ -7,8 +7,8 @@ var parkbook = angular.module("parkbook", [
     parkbook.config(['$urlRouterProvider', '$stateProvider', 'ezfbProvider', function ($urlRouterProvider, $stateProvider, ezfbProvider) {
         $urlRouterProvider.otherwise('/');
 
-        //var url = "http:localhost:3000";
-        var url = "https://parkbook.herokuapp.com";
+        var url = "http:localhost:3000";
+        //var url = "https://parkbook.herokuapp.com";
 
 
         $stateProvider
@@ -37,6 +37,22 @@ var parkbook = angular.module("parkbook", [
                             return response;
                         })
                     }]
+                }
+            })
+            .state('admin',{
+                url:'/admin',
+                templateUrl:'views/admin.html',
+                controller:'AdminCtrl',
+                resolve: {
+                    admin:
+                        ['$http','$stateParams', function($http, $stateParams){
+                            //get park from server RESTFUL API
+                            return $http.get(url + '/loadpark/' + $stateParams.parkName, {name:$stateParams.parkName}).success(function(response){
+                                console.log($stateParams);
+                                console.log($stateParams.parkName + " inside app.js");
+                                return response;
+                            })
+                        }]
                 }
             });
 
