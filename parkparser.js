4f10d9da63d06c3b38d0be51720f2ec37fe0408c
Ha Nguyen <nguyenhasc@gmail.com>
2015-07-16 17:05:39 -0700

    added test and refactored

--- parkparser.js
+++ parkparser.js
@@ -18,6 +18,9 @@ var ftp = new JSFtp( {
 var filename = 'parkdata.xml';
 var localpath = appRoot + '/data/temp/' + filename;
 
+var fs = require('fs'),
+    xml2js = require('xml2js');
+var filePath = 'data/temp/parkdata.xml';
 
 function downloadData() {
     ftp.get('opendata/xml/parks_facilities.xml', localpath, function(err) {
@@ -37,63 +40,99 @@ function clearData() {
       }
     })
 }
+function getFacility(parkEntry) {
+    var facilityType = [];
+    try {
+        var facility = parkEntry.Facilities[0].Facility;
+        for (var j = 0; j < facility.length; j++) {
+            facilityType[j] = facility[j].FacilityType[0];
+        }
+    }
+    catch (err) {
+        console.log("no facilities");
+        facilityType[0] = "No facilities found";
+    }
 
+    return facilityType;
+
+}
+
+function getFeatures(parkEntry) {
+    var features = [];
+    try {
+        var specialFeature = parkEntry.Facilities[0].SpecialFeature;
+        for (var j = 0; j < specialFeature.length; j++) {
+            features[j] = specialFeature[j];
+        }
+    }
+    catch (err) {
+        console.log("no facilities");
+        features[0] = "No features found";
+    }
+
+    return features;
+
+}
+
+function getWashroomLocation(parkEntry) {
+    var washroomLocation = [];
+    try {
+        var washroom = parkEntry.Facilities[0].Washroom;
+        for (var p = 0; p < washroom.length; p++) {
+            washroomLocation[p] = washroom[p].Location[0];
+        }
+    }
+    catch (err) {
+        console.log("no washrooms");
+        washroomLocation[0] = "No washrooms found";
+    }
+
+    return washroomLocation;
+
+}
+
+function writeFileLocal(file, result) {
+
+    try {
+        jf.writeFile(file, result);
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+    return result;
+
+}
+
+function createPark(parkEntry){
+    var parkName = parkEntry.Name[0];
+    var parkID = parkEntry.$.ID;
+    var streetNumber = parkEntry.StreetNumber[0];
+    var streetName = parkEntry.StreetName[0];
+    var destination = parkEntry.GoogleMapDest[0];
+    var res = destination.split(",");
+    var lat = res[0];
+    var lon = res[1];
+
+    var facilityType = getFacility(parkEntry);
+    var features = getFeatures(parkEntry);
+    var washroomLocation = getWashroomLocation(parkEntry);
+
+    var park = new Park({
+        name: parkName,
+        parkID: parkID,
+        streetNumber: streetNumber,
+        streetName: streetName,
+        lat: parseFloat(lat),
+        lon: parseFloat(lon),
+        facilityType: facilityType,
+        washroomLocation: washroomLocation,
+        features: features
+    });
+    return park;
+}
 function parseData() {
     //put this part in a new mapping function
-    var fs = require('fs'),
-        xml2js = require('xml2js');
-    var filePath = 'data/temp/parkdata.xml';
-
-    function getFacility(parkEntry) {
-        var facilityType = [];
-        try {
-            var facility = parkEntry.Facilities[0].Facility;
-            for (var j = 0; j < facility.length; j++) {
-                facilityType[j] = facility[j].FacilityType[0];
-            }
-        }
-        catch (err) {
-            console.log("no facilities");
-            facilityType[0] = "No facilities found";
-        }
-
-        return facilityType;
-
-    }
-
-    function getFeatures(parkEntry) {
-        var features = [];
-        try {
-            var specialFeature = parkEntry.Facilities[0].SpecialFeature;
-            for (var j = 0; j < specialFeature.length; j++) {
-                features[j] = specialFeature[j];
-            }
-        }
-        catch (err) {
-            console.log("no facilities");
-            features[0] = "No features found";
-        }
-
-        return features;
-
-    }
-
-    function getWashroomLocation(parkEntry) {
-        var washroomLocation = [];
-        try {
-            var washroom = parkEntry.Facilities[0].Washroom;
-            for (var p = 0; p < washroom.length; p++) {
-                washroomLocation[p] = washroom[p].Location[0];
-            }
-        }
-        catch (err) {
-            console.log("no washrooms");
-            washroomLocation[0] = "No washrooms found";
-        }
-
-        return washroomLocation;
-
-    }
 
     try {
         var fileData = fs.readFileSync(filePath, 'ascii');
@@ -102,41 +141,42 @@ function parseData() {
         parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
             var file = 'data/temp/parks.json';
 
-            jf.writeFile(file, result, function(err) {
-                console.log(err)
-            });
-
+            //jf.writeFile(file, result, function(err) {
+            //    console.log(err)
+            //});
+            //var fileJSON = writeFileLocal(file, result);
             var count = 0;
 
             var parkArray = result.COVParksFacilities.Park;
             console.log(parkArray);
             for (var i=0; i< parkArray.length; i++) {
                 var parkEntry = parkArray[i];
-                var parkName = parkEntry.Name[0];
-                var parkID = parkEntry.$.ID;
-                var streetNumber = parkEntry.StreetNumber[0];
-                var streetName = parkEntry.StreetName[0];
-                var destination = parkEntry.GoogleMapDest[0];
-                var res = destination.split(",");
-                var lat = res[0];
-                var lon = res[1];
 
-                var facilityType = getFacility(parkEntry);
-                var features = getFeatures(parkEntry);
-                var washroomLocation = getWashroomLocation(parkEntry);
-
-                var park = new Park({
-                    name: parkName,
-                    parkID: parkID,
-                    streetNumber: streetNumber,
-                    streetName: streetName,
-                    lat: parseFloat(lat),
-                    lon: parseFloat(lon),
-                    facilityType: facilityType,
-                    washroomLocation: washroomLocation,
-                    features: features
-                });
-
+                //var parkName = parkEntry.Name[0];
+                //var parkID = parkEntry.$.ID;
+                //var streetNumber = parkEntry.StreetNumber[0];
+                //var streetName = parkEntry.StreetName[0];
+                //var destination = parkEntry.GoogleMapDest[0];
+                //var res = destination.split(",");
+                //var lat = res[0];
+                //var lon = res[1];
+                //
+                //var facilityType = getFacility(parkEntry);
+                //var features = getFeatures(parkEntry);
+                //var washroomLocation = getWashroomLocation(parkEntry);
+                //
+                //var park = new Park({
+                //    name: parkName,
+                //    parkID: parkID,
+                //    streetNumber: streetNumber,
+                //    streetName: streetName,
+                //    lat: parseFloat(lat),
+                //    lon: parseFloat(lon),
+                //    facilityType: facilityType,
+                //    washroomLocation: washroomLocation,
+                //    features: features
+                //});
+                var park = createPark(parkEntry);
                 park.save();
                 count++;
 
@@ -154,5 +194,6 @@ function parseData() {
 module.exports = {
     downloadData : downloadData,
     parseData : parseData,
+    createPark : createPark,
     clearData : clearData
 };
--- test/parsertest.js
+++ test/parsertest.js
@@ -1,22 +1,47 @@
 
 var assert = require('assert');
-
 var expect = require('chai').expect;
 
 var fs = require('fs'),
     xml2js = require('xml2js');
 var filePath = 'data/temp/testdata.xml';
+var Parser = require('../parkparser.js');
 
-
-describe("Parser Tests", function() {
-    describe('Park.parser', function() {
+describe("XMLtoJSON", function() {
         it('this should return us a park in json format', function () {
             var fileData = fs.readFileSync(filePath, 'ascii');
             var parser = new xml2js.Parser();
             parser.parseString(fileData, function (err, result) {
-                expect(JSON.stringify(result)).to.equal( '{"park":{"Name":["Arbutus Village Park"]}}');
+                expect(JSON.stringify(result)).to.equal( '{"park":{"$":{"ID":"1"},"Name":["Arbutus Village Park"],"Official":["1"],"StreetNumber":["4202"],"StreetName":["Valley Drive"],"EWStreet":["King Edward Avenue"],"NSStreet":["Valley Drive"],"GoogleMapDest":["49.249783,-123.155250"],"Hectare":["1.41"],"Neighbourhood":[{"NeighbourhoodName":["Arbutus Ridge"],"NeighbourhoodURL":["http://vancouver.ca/community_profiles/arbutus_ridge/index.htm"]}],"Advisories":[""],"Facilities":[{"Facility":[{"FacilityCount":["1"],"FacilityType":["Playgrounds"],"FacilityURL":[""]}],"Washroom":[""]}]}}');
             });
         });
+});
+
+describe("GetInfoFromParks to Save", function() {
+    it('this should return us a park in json format with parkModel Params', function (done){
+    //before(function () {
+            var fileData = fs.readFileSync(filePath, 'ascii');
+            var parser = new xml2js.Parser();
+            parser.parseString(fileData, function (err, result) {
+                var jsonstring = Parser.createPark(result.park);
+                console.log(JSON.stringify(jsonstring));
+                expect(JSON.stringify(jsonstring)).to.equal( '{"name":"Arbutus Village Park","parkID":"1","streetNumber":"4202","streetName":"Valley Drive","lat":49.249783,"lon":-123.15525,"facilityType":["Playgrounds"],"washroomLocation":["No washrooms found"],"features":["No features found"]}');
+                done();
+        });
     });
+
+    //describe('Park.getPark', function() {
+    //    it('should show get parkinfo', function () {
+    //        Parser.createPark("Yves's Room",function (err, park) {
+    //            expect(err).to.eql(null);
+    //            expect(park.name).to.equal("Yves's Room");
+    //            expect(park.streetNumber).to.equal("6969");
+    //            expect(park.streetName).to.equal('Somewhere Richmond');
+    //            done();
+    //        });
+    //    });
+    //});
+
+
 
 });
